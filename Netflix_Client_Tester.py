10096935b7e8ee4b7a087769d8979901942f53f8
Michael Chun <mikechun@gmail.com>
2011-06-07 01:46:16 -0700

    added bunch of test codes

--- Netflix_Client_Tester.py
+++ Netflix_Client_Tester.py
@@ -1,10 +1,8 @@
-'''
-Created on Jun 5, 2011
+#! /usr/bin/python
 
-@author: jeongjin
-'''
 import sys
 import socket
+import ConfigParser
 from threading import Timer
 
 from string import atoi
@@ -12,9 +10,10 @@ from string import atoi
 #exit_function
 def client_exit(rcount,pcount,sock):
     sock.close()
-    print `rcount` + '/' + `pcount`
+    print '%d / %d'%(rcount, pcount)
     sys.exit();
     
+print ('starting client........')
 #config_path
 config_path = ""
 if len(sys.argv) > 1 :
@@ -22,43 +21,33 @@ if len(sys.argv) > 1 :
 else :
     config_path = "Netflix_Client.cfg"
 
-#config loading    
-f = open(config_path,'r')
-lines = f.readlines()
-f.close()
+config = ConfigParser.RawConfigParser()
+config.read(config_path)
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 0;
-packet_count = 0;
-time_out = 0;
-
-for line in lines: 
-    if line[0] == '#' or line[0] == '\n' :
-        continue
-    params = line.split(',')
-    if len(params) != 3:
-        print "Config Format Error"
-        sys.exit()
-    
-    UDP_PORT = atoi(params[0])
-    packet_count =atoi(params[1])
-    time_out = atoi(params[2])
+ip = config.get('Client', 'ip')
+port = config.getint('Client', 'port')
+timeout = config.getint('Client', 'timeout')
+packet_count = config.getint('Client', 'packet_count')
+print 'Starting client at %s:%d with timeout:%d'%(ip, port, timeout)
 
 sock = socket.socket( socket.AF_INET, # Internet
                       socket.SOCK_DGRAM ) # UDP
-sock.settimeout(time_out)
-sock.bind( (UDP_IP,UDP_PORT) )
+sock.settimeout(timeout)
+sock.bind((ip,port))
 
 recv_count = 0
 
+print 'Waiting for packet...'
 while True:
     try:
         data, addr = sock.recvfrom( 1024 ) # buffer size is 1024 bytes
     except socket.timeout:
         client_exit(recv_count, packet_count, sock)
         
-    recv_count = recv_count + 1        
+    recv_count += 1        
+    print '.',
     if recv_count == packet_count :
+	print 'Received %d packets'% packet_count
         client_exit(recv_count, packet_count, sock)    
     #print "received message:", data
 
--- Netflix_Server_Tester.py
+++ Netflix_Server_Tester.py
@@ -1,25 +1,22 @@
-'''
-Created on Jun 5, 2011
+#! /usr/bin/python
 
-@author: jeongjin
-'''
 import sys
 import socket
+import ConfigParser
+import string
 from threading import Timer
 from string import atoi, atof
 
 #udp send_packet function
-def send_packet(id):
-    UDP_IP = destination[id][0]
-    UDP_PORT = atoi(destination[id][1])
-    pcount = packet_count[id]
-    
+def send_packet(udp_ip, udp_port, pcount):
+    count = 0
     while(pcount) :
         pcount = pcount - 1
         sock = socket.socket( socket.AF_INET, # Internet
                               socket.SOCK_DGRAM ) # UDP
-        sock.sendto( `packet_count[id] - pcount`, (UDP_IP, UDP_PORT) )
-        print UDP_IP + ':' + `UDP_PORT` + ":" + `packet_count[id] - pcount` + "th packet sent"
+        sock.sendto(str(count), (udp_ip, udp_port) )
+        count+=1
+        print udp_ip + ':' + str(udp_port) + ":" + str(count) + "th packet sent"
 
 #config_path
 config_path = ""
@@ -28,30 +25,23 @@ if len(sys.argv) > 1 :
 else :
     config_path = "Netflix_Server.cfg"
 
-#config loading    
-f = open(config_path,'r')
+config = ConfigParser.RawConfigParser()
+config.read(config_path)
+
+ip = config.get('Server', 'ip')
+port = config.getint('Server', 'port')
+
+print 'Starting server at %s:%d'%(ip, port)
+
+#read play log
+f = open('Netflix_Server.log')
 lines = f.readlines()
 f.close()
 
-id = 0;
-destination = {}
-packet_count = {}
-
-#parse config file and schedule sending
-for line in lines: 
-    if line[0] == '#' or line[0] == '\n' :
+for line in lines:
+    if line[0] == '#' or line[0] == string.whitespace:
         continue
     params = line.split(',')
     if len(params) != 4:
-        continue 
-    
-    destination[id] = params[0:2]
-    packet_count[id] = atoi(params[3]) 
-
-    Timer(atof(params[2]),send_packet,[id]).start();
-    id = id + 1
-
-    
-    
-    
-    
+        continue
+    Timer(atof(params[2]),send_packet,[params[0],int(params[1]),int(params[3])]).start();
