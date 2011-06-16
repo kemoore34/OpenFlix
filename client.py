f67a2308b15c0f13a6d4131eac54bf1d2d685916
Jeongjin Ku <jeongjin.ku@gmail.com>
2011-06-16 04:59:49 -0700

    server & client update
    Change-Id: I8b3cb21c92559ef07b15525fd5274c6bdb43c3d6

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
 
-import socket, sys
-
+import socket, sys, time
+from string import atoi
 #exit_function
 class Client:
     def __init__(self, name, ip, port, timeout):
@@ -13,6 +13,10 @@ class Client:
         self.f = open(self.log_name, "w")
         self.num_packets = 0
         self.recv_count = 0
+        self.rate_recv_count = 0
+        self.rate = 0;
+        self.return_ip = ""
+        self.return_port = 0;
 
     def client_exit(self, rcount,pcount):
         self.sock.close()
@@ -32,19 +36,45 @@ class Client:
 
         self.f.write('Waiting for packet...\n')
         
+        last_time = 0;
         while True:
             try:
                 data, addr = self.sock.recvfrom( 1024 ) # buffer size is 1024 bytes
+                
+                self.rate_recv_count +=1
+                self.recv_count += 1
+                
+                if last_time==0 :
+                    last_time = time.time()
+                else :
+                    new_time = time.time()
+                    
+                    if (new_time - last_time) >= 1 :
+                        if self.rate_recv_count < (0.8*self.rate) :
+                            #print str(self.rate_recv_count) + " < " +str(0.8*self.rate)
+                            temp_sock = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
+                            temp_sock.setblocking(0)
+                            temp_sock.sendto("low",(self.return_ip,self.return_port))
+                            temp_sock.close()
+                        self.rate_recv_count = 0
+                        last_time = new_time
+                        
+                seq_total = data.strip()
+                #print seq_total
+                seq, self.num_packets, self.rate = map(int, seq_total.split('/')[:3])
+                
+                url = seq_total.split('/')[3].split(':');
+                self.return_ip = url[0];
+                self.return_port = atoi(url[1]);
+                
+                # Terminate client
+                if seq == self.num_packets:
+                    self.client_exit(self.recv_count, self.num_packets)
+                     
             except socket.timeout:
                 self.client_exit(self.recv_count, self.num_packets)
                 
-            self.recv_count += 1        
-            seq_total = data.strip()
-            seq, self.num_packets = map(int, seq_total.split('/'))
-            
-            # Terminate client
-            if seq == self.num_packets:
-                self.client_exit(self.recv_count, self.num_packets)    
+                
 
 #config_path
 config_path = ""
@@ -53,7 +83,7 @@ if len(sys.argv) > 1 :
     port = int(port)
 else :
     ip = '127.0.0.1'
-    port = 80
+    port = 1234 
 
 name = ip
 timeout = 30
---
+++ server_new.py
@@ -0,0 +1,79 @@
+'''
+Created on Jun 5, 2011
+
+@author: jeongjin
+'''
+import sys,time,socket
+from threading import Thread
+from string import atoi
+
+class ReqGenerator(Thread):
+    def __init__(self, self_ip, self_port, ip, port, rate, pcount):
+        Thread.__init__(self)
+        
+        self.selfip_ = self_ip;
+        self.selfport_ =self_port;
+        
+        self.ip_= ip;
+        self.port_= port;
+        self.rate_= rate;
+        self.pcount_= pcount;
+    
+    def run(self):
+        
+        sleep_time = 1. / self.rate_
+        sd = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sd.setblocking(0)
+        #sd.connect((self.ip_,self.port_))
+        
+        sdr = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sdr.bind((self.selfip_,self.selfport_))
+        sdr.setblocking(0)
+        packet_len = 100
+        sent_count = 1;
+        while(sent_count <= self.pcount_):
+            time.sleep(sleep_time)
+            try:
+                rdata = sdr.recv(1000)
+                if rdata == 'low':
+                    #self.rate_ = (int) (0.8 * self.rate_)
+                    #sleep_time = 1. / self.rate_
+                    packet_len = 50; 
+                    #print "rate is decreased. new rate is" + str(self.rate_)
+                    print "quality is decreased. new quality length is " + str(50)
+            except:
+                pass
+            
+            
+            msg = str(sent_count)+'/'+str(self.pcount_) +'/'+str(self.rate_) +'/'+self.selfip_+":"+str(self.selfport_)
+            sdata = ' '*(packet_len - len(msg)) + msg
+            
+            try:
+                sd.sendto(sdata,(self.ip_,self.port_))
+                print str(sent_count) + "th packet sent"
+                sent_count = sent_count + 1
+            except:
+                print "packet dropped. exit."
+                sys.exit()
+            
+        
+if __name__ == '__main__':
+    if (sys.argv[0].find("pydoc") == -1):
+        if (len(sys.argv) < 2 or
+            sys.argv[1] == "--help" or
+            sys.argv[1] == "-h"):
+                print(sys.argv[0]+" URL_SELF URL_TO [rate] [number of packets = 20]")
+                print(sys.argv[0]+"     URL_SELF: ip_address:port.  Ex. 127.0.0.1:80.")
+                print(sys.argv[0]+"     URL_TO: ip_address:port.  Ex. 127.0.0.1:80.")
+                print(sys.argv[0]+"     rate: packets per second.  -1 sends at maximum rate.")
+                sys.exit(1)
+                
+    uri_self = (sys.argv[1]).split(":")
+    uri = (sys.argv[2]).split(":")
+    rate = int(sys.argv[3])
+    num_packets = int(sys.argv[4])
+   
+    rg = ReqGenerator(uri_self[0],atoi(uri_self[1]),uri[0],atoi(uri[1]),rate,num_packets)
+    rg.start();
+    
+    
